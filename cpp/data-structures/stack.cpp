edc4c7d3d11c83481b1b4b4bcb396cd4a6c865cb
capitalists <amponsah735@gmail.com>
2020-12-12 22:37:31 +0000

    Create stack.cpp

---
+++ cpp/data-structures/stack.cpp
@@ -0,0 +1,45 @@
+#include "stack.h"
+
+template <typename T>
+Node<T> Stack<T>::pop() {
+	std::shared_ptr<Node> x = top;
+	top = top->next;
+	--size;
+	return *x;
+}
+
+template <typename T>
+void Stack<T>::push(std::shared_ptr<Node<T>> newTop) {
+	newTop->next = top;
+	top = newTop;
+	++size;
+}
+template <typename T>
+void Stack<T>::push(Node<T> &newTop) {
+	newTop.next = top;
+	top = std::make_shared<Node<T>>(newTop);
+	++size;
+}
+template <typename T>
+bool Stack<T>::isEmpty() {
+	return top != nullptr;
+}
+template <typename T>
+size_t Stack<T>::getSize() {
+	return size;
+}
+template <typename T>
+bool Stack<T>::hasValue(T val) {
+	std::shared_ptr<Node<T>> iter = top;
+	while (iter != nullptr) {
+		if (iter->val == val)
+			return true;
+		iter = iter->next;
+	}
+	return false;
+}
+
+template <typename T>
+Node<T>* Stack<T>::begin() {
+	return top;
+}
