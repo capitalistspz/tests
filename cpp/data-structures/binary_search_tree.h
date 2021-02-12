291eb2b80303a9ef9480448ec0e5d4b2328f27c1
capitalists <amponsah735@gmail.com>
2021-02-12 19:07:29 +0000

    Optimisations and fixes
    + Switched over to using double pointers for traversal for optimisation (it's probably faster than more if statements).
    + Rebalance method to change the node order for faster searches
    - Made the tree uncopiable -> no node sharing thank you very much

--- cpp/data-structures/binary_search_tree.h
+++ cpp/data-structures/binary_search_tree.h
@@ -1,88 +1,88 @@
 #pragma once
 #include <vector>
+#include <cstdlib>
+#include <functional>
 template <typename T>
 struct bst_node {
 	bst_node(T item) {
 		value = item;
 	}
 	// Child nodes
-	bst_node<T> *left = nullptr, *right = nullptr, *parent;
+	bst_node<T> *left = nullptr, *right = nullptr, *parent = nullptr;
 
 	// Value held by node
 	T value;
 };
 template <typename T>
+// Binary search tree. Only works with comparable types
 struct bst {
 	bst(T item) {
 		root = new bst_node<T>(item);
-		++count;
+		count = 1;
 	}
 	bst() { root = nullptr; }
+	
+	// Prevent the tree from being copied
+	// Takes O(0) time because the function doesn't even exist during runtime
+	bst(const bst<T>& other) = delete;
+	void operator=(const bst<T>& other) = delete;
+
+	/// Returns whether the structure is empty
+	// Takes O(1) time.
+	bool empty() { return !root; }
 
 	// Append a value to the binary search tree
-	// Time complexity is probably O(h) where h is height of tree
 	void add(const T item) {
-		if (root) {
-			bst_node<T> *iter = root, *prev = nullptr;
-			bool less;
-			// Traverse until reaching an empty space, and insert the value in that empty space
-			while (iter != nullptr) {
-				prev = iter; // Parent node
-				less = (item < iter -> value);
-				iter = (less) ? iter -> left : iter -> right;
-			}
-			if (less){
-				prev -> left = new bst_node<T>(item);
-				iter = prev->left;
-			}
-			else {
-				prev -> right = new bst_node<T>(item);
-				iter = prev->right;
-			}
-			iter->parent = prev;
+		bst_node<T> **iter = &root, *prev = nullptr;
 
+		// Traverse until reaching an empty space, and insert the value in that empty space
+		while (bst_node<T> *temp =  *iter) {
+			// Parent node
+			prev = *iter;
+
+			// Go left if lesser, else go right
+			iter = (item < temp->value) ? &temp->left : &temp->right;
 		}
-		else {
-			root = new bst_node<T>(item);
-		}
+		*iter = new bst_node<T>(item);
+		(*iter)->parent = prev;
 		++count;
 	}
+
 	// Find value and return pointer to it
-	T* find(const T item) {
-		auto temp = find_node_of(item);
-		// Return nullptr if not found
+	const T* find(const T item) {
+		bst_node<T> temp = *find_node_of(item);
+		// Return the address of the value or return nullptr
 		return (temp) ? &(temp->value): nullptr;
 	}
+
 	// Remove a single instance of a value from the binary search tree
-	void remove(T item) {
+	void remove(const T item) {
 		// Find node of the value
-		bst_node<T>* ptr = find_node_of(item, root);
+		bst_node<T>** ptr = find_node_of(item, root);
 		// If it wasn't found do nothing
-		if (!ptr) return;
-		// Traverse down to the lowest value of right hand side of value node
-		bst_node<T>* right = find_min_from(ptr->right);
+		if (bst_node<T>* selected = *ptr) {
+			// Traverse down to the lowest value of right hand side of value node
+			bst_node<T> **low_right = find_min_from(selected->right);
 
-		/* If the item is at the root, the right node becomes the root,
-			the furthest down on the right tree links to the left of the original
-		*/
-		if (ptr == root) {
-			root = ptr->right;
-			right->left = ptr->left;
-		}
+			// The furthest down on the right tree now links to the left of the original
+			*low_right = selected->left;
 
-		else {
-			// Get parent of value node
-			bst_node<T>* parent = (ptr->parent);
-			// Assign the position of the original to the original's right side
-			if (parent->left == ptr)
-				parent->left = right;
-			else
-				parent->right = right;
-		}
-		delete ptr;
+			// The node is replace by its right
+			*ptr = selected->right;
+			delete selected;
+
+			--count;
+		};
+		
+	}
+	/// Number of elements in binary search tree.
+	// Takes O(1) time.
+	std::size_t size() const {
+		return count;
 	}
 
 	// Finds all values in the tree and outputs them to a an ordered vector
+	// Takes O(n) time.
 	std::vector<T> to_vector() {
 		std::vector<T> arr;
 		std::function<void(bst_node<T>*)> collect = [&collect, &arr]
@@ -95,38 +95,89 @@ struct bst {
 		}; collect(root);
 		return arr;
 	}
+	
+	// Rebalances the tree for faster traversal
+	void rebalance() {
+		auto size = count;
+		T *ordered = new T[size];
+		std::size_t num = 0;
+
+		// Removes all elements and adds them to an array in value order
+		std::function<void(bst_node<T>*)> pop = [&num, &pop, ordered]
+		(bst_node<T>* node) {
+			if (node) {
+				pop(node->left);
+				*(ordered + num++) = node->value;
+				pop(node->right);
+				delete node;
+			}
+		}; pop(root);
+
+		// Add the values into the new tree
+		std::function<bst_node<T>* (std::size_t, std::size_t)> arr_to_bst = [ordered, &arr_to_bst]
+		(std::size_t left, std::size_t right) {
+			const auto size = right - left;
+
+			// Middle index for section
+			const auto mid = left + size / 2;
+			
+			// Create a node for value
+			bst_node<T>* point = new bst_node<T>(ordered[mid]);
+
+			// No splitting of single nodes
+			if (size > 1) {
+				point->left = arr_to_bst(left, mid);
+				point->right = arr_to_bst(mid + 1, right);
+			}
+
+			return point;
+		};
+		root = arr_to_bst(0, count);
+
+	}
+	~bst() {
+		clear(root);
+	}
 // Internal methods
 private:
 	// Find the node that holds that value and return a pointer to it
-	// Time complexity is probably O(log n)
-	bst_node<T>* find_node_of(const T item, bst_node<T>* root) {
-		auto iter = root;
-		while (iter != nullptr) {
-			// If value is found, return pointer to value
-			if (iter -> value == item) break;
-
+	bst_node<T>** find_node_of(const T item, bst_node<T>* local_root) {
+		bst_node<T> **iter = &local_root;
+		bst_node<T>* current = *iter;
+		// Run until nullptr or value
+		while (current && (current->value != item)) {
 			// If the item's value is less than the pivot's, go left, if it is more than or equal, go right
-			iter = (item < iter -> value) ? iter -> left : iter -> right;
-
+			iter = (item < current->value) ? &(current->left) : &(current->right);
+			current = *iter;
 		}
 		return iter;
 	}
 	// Continually traverses left from that starting point, returning the last non-null node
-	// Time complexity is probably O(h)
-	bst_node<T>* find_min_from(bst_node<T>* ptr) {
-		while (ptr -> left)
+	bst_node<T>** find_min_from(bst_node<T>* ptr) {
+		while (&ptr->left)
 			ptr = ptr -> left;
-		return ptr;
+		return &ptr;
 	}
 	// Continually traverses right from that starting point, returning the last non-null node
-	// Time complexity is probably O(h)
 	bst_node<T>* find_max_from(bst_node<T>* ptr) {
 		while (ptr -> right)
 			ptr = ptr -> right;
 		return ptr;
 	}
+
+	// Remove all nodes from the tree.
+	// Takes O(n) time.
+	void clear(bst_node<T>* node) {
+		if (node) {
+			clear(node->left);
+			clear(node->right);
+			delete node;
+		}
+		count = 0;
+	}
 // Private fields
 private:
-	bst_node<T>* root;
 	std::size_t count = 0;
+protected:
+	bst_node<T>* root;
 };
